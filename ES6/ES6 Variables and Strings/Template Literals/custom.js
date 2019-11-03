8e2031678fc9a37ca26bf88798627c423b1f344c
saif43 <saif.ahmed.anik.0@gmail.com>
2019-11-04 00:02:34 +0600

    upload

---
+++ ES6/ES6 Variables and Strings/Template Literals/custom.js
@@ -0,0 +1,10 @@
+let name = 'Saif Ahmed Anik';
+
+let sentence = `My name is ${name}`;
+
+document.write(sentence+'<br>');
+
+let a = 8;
+let b = 34;
+let msg = `The sum is ${a+b}`;
+document.write(msg);
