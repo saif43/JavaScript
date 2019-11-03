2cdc76b2747cfcc307caa9895b959d4ec7b6c67c
saif43 <saif.ahmed.anik.0@gmail.com>
2019-11-03 21:35:52 +0600

    change

--- Image Slider/slider.js
+++ Image Slider/slider.js
@@ -1,33 +1,51 @@
-  var images = [
-  "http://www.sololearn.com/uploads/slider/1.jpg", 
-  "http://www.sololearn.com/uploads/slider/2.jpg", 
+var images = [
+  "http://www.sololearn.com/uploads/slider/1.jpg",
+  "http://www.sololearn.com/uploads/slider/2.jpg",
   "http://www.sololearn.com/uploads/slider/3.jpg"
-  ];
-  var num = 0;
+];
+var num = 0;
+
+
 
 function next() {
   var slider = document.getElementById("slider");
   num++;
-  if(num >= images.length) {
+  if (num >= images.length) {
     num = 0;
   }
   slider.src = images[num];
-  }
+}
 
 function prev() {
   var slider = document.getElementById("slider");
   num--;
-  if(num < 0) {
-    num = images.length-1;
+  if (num < 0) {
+    num = images.length - 1;
   }
   slider.src = images[num];
 }
 
-function halfPrice(){
+function halfPrice() {
   var price = document.getElementById("price");
 
-  price.innerHTML = Math.round(price.innerHTML/2);
+  price.innerHTML = Math.round(price.innerHTML / 2);
 
 }
 
-addEventListener("click",halfPrice,true);
+function doublePrice() {
+  var price = document.getElementById("price");
+
+  price.innerHTML = Math.round(price.innerHTML * 2);
+
+}
+
+window.onload = function () {
+  var prevBtn = document.getElementById("prev");
+  var nextBtn = document.getElementById("next");
+
+  // addEventListener("click", halfPrice, true);
+  prevBtn.addEventListener("click", halfPrice, true);
+  prevBtn.addEventListener("click", prev, true);
+  nextBtn.addEventListener("click", doublePrice, true);
+  nextBtn.addEventListener("click", next, true);
+}
